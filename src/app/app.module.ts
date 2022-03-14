21b0dd937c34c4dd51c2ebc6eb90e4608ce7808c
carlosjorge7 <carlosjorge7@gmail.com>
2022-03-14 10:50:14 +0100

    modulos usuarios y productos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsuariosFormComponent } from './usuarios/componentes/usuarios-form/usuarios-form.component';
+import { ProductosFormComponent } from './productos/componentes/productos-form/productos-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsuariosFormComponent,
+    ProductosFormComponent
   ],
   imports: [
     BrowserModule,
